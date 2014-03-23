4a7179162cf476b8c5d9d30710342c2e6c4cd2da
Craig Treulieb <ctreulieb@gmail.com>
2014-03-22 20:31:32 -0400

    Here goes nothin

---
+++ Yahtzee/YahtzeeLibrary/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+namespace YahtzeeLibrary
+{
+    public interface ICallBack
+    {
+
+    }
+
+    [ServiceContract(CallbackContract = typeof(ICallBack))]
+    public interface IGame {
+
+    }
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Game : IGame
+    {
+        private List<Player> players = new List<Player>();
+    }
+}
---
+++ Yahtzee/YahtzeeService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using YahtzeeLibrary;
+
+namespace YahtzeeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                ServiceHost servHost = new ServiceHost(typeof(Game));
+
+                Console.WriteLine("Yahtzee game service started, Press <Enter> to quit");
+                Console.ReadKey();
+
+                servHost.Close();
+            }catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
