6191c0235dc2f603f4aa217b00b554bb1e44629b
Craig Treulieb <ctreulieb@gmail.com>
2014-04-03 13:57:53 -0400

    adding YahtzeeContracts

---
+++ Yahtzee/YahtzeeContracts/GameState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace YahtzeeContracts
+{
+    [DataContract]
+    public class GameState
+    {
+        [DataMember]
+        public Player[] players { get; private set; }
+
+        [DataMember]
+        public int[] dice { get; private set; }
+
+        [DataMember]
+        public int turnID { get; private set; }
+
+        public GameState(Player[] p, int[] d, int t)
+        {
+            dice = d;
+            players = p;
+            turnID = t;
+        }
+    }
+}
